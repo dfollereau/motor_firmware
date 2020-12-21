21d8963ffe839eba486e65d7ba4e16d50fdf4f4f
Denis FOLLEREAU <denis.follereau06@gmail.com>
2020-12-21 15:40:29 +0100

    Update generate_files.sh

--- protobuf/generate_files.sh
+++ protobuf/generate_files.sh
@@ -1,2 +1,2 @@
-protoc --nanopb_out=. ./steppermotors.proto
-protoc --python_out=. ./steppermotors.proto
+protoc -Iproto -I. --python_out=. proto_files/steppermotors.proto
+protoc -Iproto -I. --nanopb_out=. proto_files/steppermotors.proto
